3492d188d08d4c79a59c39428fea021335b81473
Alejandr0Marquez <94418140+Alejandr0Marquez@users.noreply.github.com>
2023-07-18 10:29:55 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 $server="127.0.0.1";
 $database="maquinas";
-$username="root";
-$password="";
+$username="edna";
+$password="12345";
 
 
 $con = new mysqli($server,$username,$password,$database);
